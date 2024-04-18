af15f0acc8076f58f13e782785286d036145429b
Subaiyal Sheikh <subaiyalshk@gmail.com>
2024-04-17 23:31:54 -0500

    setup material-ui theme, provider and added bottom nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import { useEffect, useState } from 'react';
 
+
 function App() {
   return (
     <div className="App">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/About" element={<About />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/About" element={<About />} />
+          </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,30 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { createTheme, ThemeProvider } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#8c2c8e',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+    background: {
+      default: '#b5b5b5',
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
