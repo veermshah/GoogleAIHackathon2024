1935583afd1cbc9b96f1d8a782e32c940b83d522
jbthelegend <jbthelegend15@gmail.com>
2024-04-26 15:17:27 -0500

    update rag folder

--- rag/create_database.py
+++ rag/create_database.py
@@ -1,7 +1,7 @@
 from langchain_community.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.embeddings import OpenAIEmbeddings
 from langchain.vectorstores.chroma import Chroma
 import os
 import shutil
