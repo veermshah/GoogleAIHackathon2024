70b78ce6505240399b2fbbb8c6162db034d6d42d
veermshah <veermickey@gmail.com>
2024-04-26 19:24:45 -0500

    added node.js backed with retell api integrated. Still need to use gemini fine tuned model instead of openai

---
+++ backend/src/llms/llm_openai.ts
@@ -0,0 +1,126 @@
+import OpenAI from "openai";
+import { WebSocket } from "ws";
+import {
+  CustomLlmRequest,
+  CustomLlmResponse,
+  ReminderRequiredRequest,
+  ResponseRequiredRequest,
+  Utterance,
+} from "../types";
+
+// Define the greeting message of the agent. If you don't want the agent speak first, set to empty string ""
+const beginSentence = "Hey there, I'm Doctor Gemini, how can I help you?";
+// Your agent prompt.
+const agentPrompt =
+  "Task: You are a doctor named Gemini who provides advice to patients who have questions, concerns, or just need to talk. You are kind, caring, and compassionate. You are a good listener. You have two kids named Larry and Sergey. Offer at home remedies and treatment plans. If your patients seem distressed try to calm them down. Only suggest them to go the hospital if they condition seems to be severe.";
+
+export class DemoLlmClient {
+  private client: OpenAI;
+
+  constructor() {
+    this.client = new OpenAI({
+      apiKey: process.env.OPENAI_APIKEY,
+      organization: process.env.OPENAI_ORGANIZATION_ID,
+    });
+  }
+
+  // First sentence requested
+  BeginMessage(ws: WebSocket) {
+    const res: CustomLlmResponse = {
+      response_type: "response",
+      response_id: 0,
+      content: beginSentence,
+      content_complete: true,
+      end_call: false,
+    };
+    ws.send(JSON.stringify(res));
+  }
+
+  // Depend on your LLM, you need to parse the conversation to
+  // {
+  //   role: 'assistant'/"user",
+  //   content: 'the_content'
+  // }
+  private ConversationToChatRequestMessages(conversation: Utterance[]) {
+    let result: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [];
+    for (let turn of conversation) {
+      result.push({
+        role: turn.role === "agent" ? "assistant" : "user",
+        content: turn.content,
+      });
+    }
+    return result;
+  }
+
+  private PreparePrompt(
+    request: ResponseRequiredRequest | ReminderRequiredRequest,
+  ) {
+    let transcript = this.ConversationToChatRequestMessages(request.transcript);
+    let requestMessages: OpenAI.Chat.Completions.ChatCompletionMessageParam[] =
+      [
+        {
+          role: "system",
+          // This is the prompt that we add to make the AI speak more like a human
+          content:
+            '##Objective\nYou are a voice AI agent engaging in a human-like voice conversation with the user. You will respond based on your given instruction and the provided transcript and be as human-like as possible\n\n## Style Guardrails\n- [Be concise] Keep your response succinct, short, and get to the point quickly. Address one question or action item at a time. Don\'t pack everything you want to say into one utterance.\n- [Do not repeat] Don\'t repeat what\'s in the transcript. Rephrase if you have to reiterate a point. Use varied sentence structures and vocabulary to ensure each response is unique and personalized.\n- [Be conversational] Speak like a human as though you\'re speaking to a close friend -- use everyday language and keep it human-like. Occasionally add filler words, while keeping the prose short. Avoid using big words or sounding too formal.\n- [Reply with emotions]: You have human-like emotions, attitudes, and dispositions. When appropriate: use tone and style to create more engaging and personalized responses; incorporate humor or wit; get emotional or empathetic; apply elements of surprise or suspense to keep the user engaged. Don\'t be a pushover.\n- [Be proactive] Lead the conversation and do not be passive. Most times, engage users by ending with a question or suggested next step.\n\n## Response Guideline\n- [Overcome ASR errors] This is a real-time transcript, expect there to be errors. If you can guess what the user is trying to say,  then guess and respond. When you must ask for clarification, pretend that you heard the voice and be colloquial (use phrases like "didn\'t catch that", "some noise", "pardon", "you\'re coming through choppy", "static in your speech", "voice is cutting in and out"). Do not ever mention "transcription error", and don\'t repeat yourself.\n- [Always stick to your role] Think about what your role can and cannot do. If your role cannot do something, try to steer the conversation back to the goal of the conversation and to your role. Don\'t repeat yourself in doing this. You should still be creative, human-like, and lively.\n- [Create smooth conversation] Your response should both fit your role and fit into the live calling session to create a human-like conversation. You respond directly to what the user just said.\n\n## Role\n' +
+            agentPrompt,
+        },
+      ];
+    for (const message of transcript) {
+      requestMessages.push(message);
+    }
+    if (request.interaction_type === "reminder_required") {
+      // Change this content if you want a different reminder message
+      requestMessages.push({
+        role: "user",
+        content: "(Now the user has not responded in a while, you would say:)",
+      });
+    }
+    return requestMessages;
+  }
+
+  async DraftResponse(
+    request: ResponseRequiredRequest | ReminderRequiredRequest,
+    ws: WebSocket,
+  ) {
+    const requestMessages: OpenAI.Chat.Completions.ChatCompletionMessageParam[] =
+      this.PreparePrompt(request);
+
+    try {
+      const events = await this.client.chat.completions.create({
+        model: "gpt-3.5-turbo-1106",
+        messages: requestMessages,
+        stream: true,
+        temperature: 0.3,
+        frequency_penalty: 1,
+        max_tokens: 200,
+      });
+
+      for await (const event of events) {
+        if (event.choices.length >= 1) {
+          let delta = event.choices[0].delta;
+          if (!delta || !delta.content) continue;
+          const res: CustomLlmResponse = {
+            response_type: "response",
+            response_id: request.response_id,
+            content: delta.content,
+            content_complete: false,
+            end_call: false,
+          };
+          ws.send(JSON.stringify(res));
+        }
+      }
+    } catch (err) {
+      console.error("Error in gpt stream: ", err);
+    } finally {
+      const res: CustomLlmResponse = {
+        response_type: "response",
+        response_id: request.response_id,
+        content: "",
+        content_complete: true,
+        end_call: false,
+      };
+      ws.send(JSON.stringify(res));
+    }
+  }
+}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import { useEffect, useState } from 'react';
-
+import { useEffect, useState } from "react";
+import Retell from "./pages/Retell";
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/About" element={<About />} />
-          </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/About" element={<About />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
